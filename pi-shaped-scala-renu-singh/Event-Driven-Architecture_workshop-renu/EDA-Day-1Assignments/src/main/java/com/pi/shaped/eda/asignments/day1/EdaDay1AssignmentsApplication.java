d4dde2dd19ddc0b25db6a799312583b4c06b5f00
rini1301 <134570581+rini1301@users.noreply.github.com>
2025-08-25 13:03:56 +0530

    Added files

---
+++ pi-shaped-scala-renu-singh/Event-Driven-Architecture_workshop-renu/EDA-Day-1Assignments/src/main/java/com/pi/shaped/eda/asignments/day1/EdaDay1AssignmentsApplication.java
@@ -0,0 +1,13 @@
+package com.pi.shaped.eda.asignments.day1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdaDay1AssignmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdaDay1AssignmentsApplication.class, args);
+	}
+
+}
