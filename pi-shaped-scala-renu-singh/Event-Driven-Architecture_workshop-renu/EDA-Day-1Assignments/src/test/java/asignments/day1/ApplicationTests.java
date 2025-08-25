2d6af03b338467fa330e539912489397ee6d57e8
rini1301 <134570581+rini1301@users.noreply.github.com>
2025-08-25 14:35:05 +0530

    Add files via upload
    made some changes in the folder

---
+++ pi-shaped-scala-renu-singh/Event-Driven-Architecture_workshop-renu/EDA-Day-1Assignments/src/test/java/asignments/day1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.shaped.eda.asignments.day1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdaDay1AssignmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
